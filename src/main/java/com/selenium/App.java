58a0b8788714240228d705fe40832e0c196ad6e2
Kshitij Baria <kshitijbb786@gmail.com>
2025-02-28 18:38:37 +0530

    adding allure

--- src/main/java/com/selenium/App.java
+++ src/main/java/com/selenium/App.java
@@ -4,7 +4,5 @@ package com.selenium;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+    public static void main(String[] args) { }
 }
